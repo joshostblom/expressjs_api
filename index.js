4e63e94dabd9122dab290f5dd5344309de252ef0
joshostblom <josh.ostblom@gmail.com>
2024-06-13 19:57:37 -0500

    Init.

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require("express")();
+const PORT = 8080;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+app.get("/resume", (req, res) => {
+  res.status(200).send({
+    tshirt: "blah",
+    size: "large",
+  });
+});
