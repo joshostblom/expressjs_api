676797c3565887be7bcde89c1ecbf07ea3f74adf
joshostblom <josh.ostblom@gmail.com>
2024-06-20 20:33:27 -0500

    Init

---
+++ database.js
@@ -0,0 +1,16 @@
+import mysql from "mysql2";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  })
+  .promise();
+
+const result = await pool.query("SELECT * FROM projects");
+console.log(result[0]);
---
+++ sql_queries/create_tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    link VARCHAR(255),
+    image VARCHAR(255)
+);
