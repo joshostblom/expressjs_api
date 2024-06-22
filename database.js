e65e99f7b5bb85bb744a45c207585be57eab264e
joshostblom <josh.ostblom@gmail.com>
2024-06-22 12:18:29 -0500

    Setup project endpoints

--- database.js
+++ database.js
@@ -12,5 +12,41 @@ const pool = mysql
   })
   .promise();
 
-const result = await pool.query("SELECT * FROM projects");
-console.log(result[0]);
+export async function getProjects() {
+  const [rows] = await pool.query("SELECT * FROM projects");
+  return rows;
+}
+
+export async function getProject(id) {
+  const [rows] = await pool.query(
+    `
+    SELECT * FROM projects
+    WHERE id = ?
+    `,
+    [id]
+  );
+  return rows[0];
+}
+
+export async function createProject(title, description, link, image) {
+  const [result] = await pool.query(
+    `
+    INSERT INTO projects (title, description, link, image)
+    VALUES (?, ?, ?, ?)
+    `,
+    [title, description, link, image]
+  );
+  return getProject(result.insertId);
+}
+
+export async function updateProject(id, title, description, link, image) {
+  const [result] = await pool.query(
+    `
+    UPDATE projects
+    SET title = ?, description = ?, link = ?, image = ?
+    WHERE id = ?
+    `,
+    [title, description, link, image, id]
+  );
+  return getProject(id);
+}
