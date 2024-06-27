9d78592fa355043a69b1e0a94f45b0faa689e3b5
joshostblom <josh.ostblom@gmail.com>
2024-06-26 20:42:06 -0500

    Install cors.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import projectRoute from "./routes/Project.js";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/project", projectRoute);
 
